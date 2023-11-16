098a797144118aa1ea4eeeb62e195a7a8c34b368
Diego González Cruz <diego@thinkey.cl>
2023-11-15 21:00:57 -0300

    checked

--- components/ModalHtml.jsx
+++ components/ModalHtml.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 const ModalHtml = ({ data, isOpen = false, onClose }) => {
   if (!isOpen) return null;
-  console.log(data, "***** data ***** ");
+
   return (
     <div className="absolute top-0 left-0 w-screen h-screen bg-black/70 z-50 flex flex-col items-center justify-center">
-      <div className="w-3/4 h-full  bg-white mx-auto rounded-xl p-20">
+      <div className="w-3/4 h-full  bg-white mx-auto rounded-xl p-20 relative">
+        <button
+          className="absolute top-5 right-5 debug1 btn btn-secondary"
+          onClick={onClose}
+        >
+          Close
+        </button>
         <div className="w-full h-full">
           <div
             className="mockup-code"
